f4ffb1d8a2f440ccfb3115bdfb5c8b5de9ac4771
AntoineTSIO <antoine.touzart.sio@gmail.com>
2022-03-09 12:55:36 +0100

    Fonction ```nb_occurences``` avec boucle ```FOR```

--- fonctions.sql
+++ fonctions.sql
@@ -13,3 +13,20 @@ $$
 
 $$
 LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION nb_occurences(caractère varchar, chaine varchar, indice_debut integer, indice_fin integer) RETURNS integer AS
+$$
+    DECLARE
+        resultat integer :=0;
+    BEGIN
+        FOR i in indice_debut..indice_fin LOOP
+            if indice_debut<length(chaine) and indice_fin<length(chaine) and indice_debut<indice_fin THEN
+                if caractère = substr(chaine,i,1) THEN
+                    resultat := resultat+1 ;
+                end if;
+            end if;
+        end loop ;
+        return resultat ;
+    END;
+$$
+LANGUAGE plpgsql;
